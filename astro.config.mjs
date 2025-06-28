a508f2894da418975be0cb489d1d86ff764653b0
Xiaomao <147717937+wamimi@users.noreply.github.com>
2025-06-28 22:07:10 +0300

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
